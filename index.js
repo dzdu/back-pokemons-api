55144a13003a186a11030779a3d7fec6f6e14b33
instdeadangel <513dragan@gmail.com>
2020-09-12 15:56:59 +0200

    added procfile

--- index.js
+++ index.js
@@ -1,39 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 //var bodyParser = require('body-parser')
-var path = require('path');
-var cors = require('cors');
-var pokemonsRouter = require('./routes/pokemonsRoute');
-var newsRouter = require('./routes/newsRoute')
-
+var path = require("path");
+var cors = require("cors");
+var pokemonsRouter = require("./routes/pokemonsRoute");
+var newsRouter = require("./routes/newsRoute");
 
 var app = express();
-var port = 3001;
-var uri = 'mongodb+srv://admin:admin@studyforfree-zbtpf.gcp.mongodb.net/Pokemons?retryWrites=true&w=majority';
+var port = process.env.PORT || 3001;
+var uri =
+  "mongodb+srv://admin:admin@studyforfree-zbtpf.gcp.mongodb.net/Pokemons?retryWrites=true&w=majority";
 
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connection.on('connected', () => {
-    console.log("connected to db");
+mongoose.connection.on("connected", () => {
+  console.log("connected to db");
 });
-mongoose.connection.on('error', (err) => {
-    console.log("NOT connected to db " + err);
-});
-
-app.listen(port, function(){
-    console.log("Nu funkar sida på port " + port);
+mongoose.connection.on("error", (err) => {
+  console.log("NOT connected to db " + err);
 });
 
+app.listen(port, function () {
+  console.log("Nu funkar sida på port " + port);
+});
 
-app.use(express.static(path.join(__dirname,'public')));
-app.use('/pokemons', pokemonsRouter);
-app.use('/news', newsRouter);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/pokemons", pokemonsRouter);
+app.use("/news", newsRouter);
 
 app.use(cors());
 app.use(express.json);
-
-
-
-
-
-
